cf1b068847dd2869e61e38d47e9577e22687ac57
Reece Wang <rew006@ucsd.edu>
2026-02-23 20:51:22 -0800

    finish discretization

--- offline_process.py
+++ offline_process.py
@@ -8,4 +8,16 @@ class Offline_Process:
         translated_df["x_~"] = translated_df["longitude"] - x_min
         translated_df["y_~"] = translated_df["latitude"] - y_min
         return translated_df, x_min, y_min
+
+    def discretization(df):
+        dis_df = df.copy()
+        c = 0.012138 #C is not provided in the paper, I just put my lucky number
+        
+        dis_df["i"] = np.floor(dis_df["x_~"] / c).astype(int)
+        dis_df["j"] = np.floor(dis_df["y_~"] / c).astype(int)
+
+        return dis_df
+        
+        
+        
     
