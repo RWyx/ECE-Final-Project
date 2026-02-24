df0d8852d030dbdc928b8a5a1a1f5ccccf45ebee
Reece Wang <rew006@ucsd.edu>
2026-02-23 20:35:37 -0800

    move libary

--- offline_process.py
+++ offline_process.py
@@ -1,9 +1,11 @@
+import pandas as pd
+import numpy as np
 class Offline_Process:
     def coordinate_translation(df):
-        import pandas as pd
         translated_df = df.copy()
         x_min = translated_df["longitude"].min()
         y_min = translated_df["latitude"].min()
         translated_df["x_~"] = translated_df["longitude"] - x_min
         translated_df["y_~"] = translated_df["latitude"] - y_min
         return translated_df, x_min, y_min
+    
